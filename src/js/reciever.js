ad2ad0fe938b769bff24517c40b60a4758992e94
Alexandre Joseph <jexhson@alexandrejoseph.com>
2011-10-25 21:17:05 -0700

    Add Sender and Reciever structure

---
+++ src/js/reciever.js
@@ -0,0 +1,8 @@
+/**
+ * Recieve packets, manage ACK and packet resend, produce stats on recieved packets.
+ */
+var RCR = function () {
+	return {
+		
+	};
+}();
---
+++ src/js/sender.js
@@ -0,0 +1,10 @@
+/**
+ * Establish connection with reciever. Send packets delay depending of the 
+ * network configuration. Manage collision, resend on error. Produce stats
+ * on transmitted packets.
+ */
+var SDR = function () {
+	return {
+		
+	};
+}();
