c1ce66209430470761b155b3b4166db4e0bd364e
Alexandre Joseph <jexhson@alexandrejoseph.com>
2011-10-25 21:16:30 -0700

    Add JS loader

---
+++ src/js/loader.js
@@ -0,0 +1,29 @@
+/**
+ * Load JavaScript libraries
+ */
+
+var JS_DIR = 'js/';
+var JS_LIBS_DIR = JS_DIR + 'libs/';
+
+Modernizr.load([{
+//  load: 'http:/­/ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js',
+//  callback: function () {
+//    if (!window.jQuery) {
+//      ({
+        load: JS_LIBS_DIR + 'jquery-1.6.4.min.js',
+        callback: function (url, result, key) {
+          console.log('Local jquery lib loaded');
+        }
+//      });
+//    }
+//  }
+}, {
+  load: [JS_DIR + 'frame.js', JS_DIR + 'sender.js', JS_DIR + 'reciever.js', JS_DIR + 'paquito.js' ],
+  callback: function (url, result, key) {
+    console.log(url + ' loaded');
+  },
+  complete: function () {
+    PKT.init();
+  }
+}]);
+
